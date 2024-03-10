9c4c9c503cf21c296e8c9ca432f67875272c772e
Steven Petryk <steven.petryk@discordapp.com>
2024-03-10 14:28:38 -0700

    Ditch worker

--- src/index.js
+++ src/index.js
@@ -1,49 +1,30 @@
-const os = require("node:os")
 const path = require("path")
 const fg = require("fast-glob")
-const { Worker } = require("node:worker_threads")
+const { processFile } = require("./processor")
 
-const numWorkers = os.cpus().length - 1
-
-class WorkerPool {
-  constructor() {
-    /** @type {any[]} */
-    this.workers = []
-  }
-
-  async run() {
-    for (let i = 0; i < numWorkers; i++) {
-      const worker = new Worker(path.join(__dirname, "worker.js"))
-      this.workers.push(worker)
-
-      worker.on("message", (message) => {
-        if (message.type === "error") {
-          console.error(message.error)
-        }
-      })
-    }
-
-    const files = fg.sync(["discord_app/**/*.module.css"], {
-      ignore: ["**/node_modules/**"],
+async function run() {
+  const files = fg.stream(
+    ["/Users/steven.petryk/src/discord/discord_app/**/*.module.css"],
+    {
+      ignore: ["**/node_modules/**", "**/dist/**", "**/bazel-discord/**"],
       cwd: path.join(__dirname, "../../discord"),
       absolute: true,
-    })
-
-    process.stderr.write(
-      `Processing ${files.length} files with ${numWorkers} workers\n`
-    )
-
-    for (const file of files) {
-      const worker = this.workers.pop()
-      worker.postMessage({ type: "process", file })
-      this.workers.unshift(worker)
     }
+  )
+  let numFiles = 0
 
-    for (const worker of this.workers) {
-      worker.postMessage({ type: "exit" })
-    }
+  const promises = []
+
+  for await (const file of files) {
+    promises.push(processFile(file.toString("utf-8")))
+    const relPath = path.relative("../discord", file.toString("utf-8"))
+    // process.stderr.write(`${relPath}\n`)
+    numFiles++
   }
+
+  await Promise.all(promises)
+
+  process.stderr.write(`\nProcessed ${numFiles} files\n`)
 }
 
-const pool = new WorkerPool()
-pool.run()
+run()
---
+++ src/processor.js
@@ -0,0 +1,112 @@
+// @ts-check
+
+const fs = require("fs/promises")
+const { SourceNode } = require("source-map")
+const { parse, Rule } = require("postcss")
+const selectorParser = require("postcss-selector-parser")
+
+const CLASSNAME_MATCHER = /\.([a-zA-Z0-9_-]+)/g
+
+/**
+ * @param {Rule} rule
+ */
+function getClassNames(rule) {
+  /** @type {any[]} */
+  let classNames = []
+  /**
+   * @type {import('postcss-selector-parser').SyncProcessor<any>}
+   */
+  const transform = (selectors) => {
+    selectors.walkClasses((node) => {
+      classNames.push(node)
+    })
+  }
+
+  selectorParser(transform).processSync(rule.selector)
+  return classNames
+}
+
+/**
+ * @param {string} file
+ */
+async function processFile(file) {
+  const content = await fs.readFile(file, "utf-8")
+
+  /** @type {import('postcss').Rule[]} */
+  const rules = []
+
+  parse(content).walkRules((rule) => {
+    const matches = rule.selector.match(CLASSNAME_MATCHER)
+
+    if (matches) {
+      rules.push(rule)
+    }
+  })
+
+  const rootNode = new SourceNode(1, 0, null, [
+    "// prettier-ignore\n",
+    "/* eslint-disable */\n",
+    "declare const styles: {\n",
+    ...rules.flatMap((rule) => {
+      const classNames = getClassNames(rule)
+
+      return classNames.flatMap((className) => {
+        const before = rule.raws.before ?? ""
+
+        let ruleSource = before + rule.toString()
+        ruleSource = ruleSource.replace(/^\n+/gm, "")
+        const indentationLevel = ruleSource.match(/^\s*/)?.[0].length ?? 0
+
+        ruleSource = ruleSource
+          // Replace lines containing only comments with \n
+          .replace(/\s*\/\*.*?\*\/\s*\n/g, "\n")
+          // Then replace all remaining comments
+          .replace(/\/\*.*?\*\//g, "")
+          .split("\n")
+          .map((line) => line.slice(indentationLevel))
+          .join("\n")
+
+        return [
+          "  /**\n",
+          "   * ```css\n",
+          ruleSource
+            .split("\n")
+            .map((line) => `   * ${line}`)
+            .join("\n") + "\n",
+          "   * ```\n",
+          "   */\n",
+          '  "',
+          new SourceNode(
+            rule.source?.start?.line ?? 1,
+            (rule.source?.start?.column ?? 1) - 1,
+            file,
+            className.toString().slice(1)
+          ),
+          new SourceNode(
+            rule.source?.end?.line ?? 1,
+            rule.source?.end?.column ?? 0,
+            file,
+            ""
+          ),
+          `\": string;\n`,
+        ]
+      })
+    }),
+    `};\n`,
+    `export default styles;`,
+  ])
+
+  const dts = file.replace(".module.css", ".module.css.d.ts")
+  const dtsMap = file.replace(".module.css", ".module.css.d.ts.map")
+
+  const newSource = rootNode.toStringWithSourceMap({ file: dts })
+
+  await Promise.all([
+    fs.writeFile(dts, newSource.code),
+    fs.writeFile(dtsMap, newSource.map.toString()),
+  ])
+
+  process.stderr.write(`${file}\n`)
+}
+
+module.exports = { processFile }
