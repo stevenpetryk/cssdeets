408ab16b72952a44217b5bcca0271dadc0c81bf3
Steven Petryk <petryk.steven@gmail.com>
2024-03-09 22:36:47 -0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_20
+
+            (pkgs.writeScriptBin "pnpm" ''
+              corepack pnpm "$@"
+            '')
+          ];
+        };
+      });
+}
