4db17070961049c069bad812a6831e86927d93ac
Steven Petryk <steven.petryk@discordapp.com>
2024-03-11 11:09:08 -0700

    wip

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ async function run() {
     ["/Users/steven.petryk/src/discord/discord_app/**/*.module.css"],
     {
       ignore: ["**/node_modules/**", "**/dist/**", "**/bazel-discord/**"],
-      cwd: path.join(__dirname, "../../discord"),
       absolute: true,
     }
   )
@@ -18,7 +17,7 @@ async function run() {
   for await (const file of files) {
     promises.push(processFile(file.toString("utf-8")))
     const relPath = path.relative("../discord", file.toString("utf-8"))
-    // process.stderr.write(`${relPath}\n`)
+    process.stderr.write(`${relPath}\n`)
     numFiles++
   }
 
